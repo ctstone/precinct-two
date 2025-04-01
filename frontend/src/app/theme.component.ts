4365fcf05d2d5777ed82554fa3d280c53b65f460
Christopher Stone <chstone@microsoft.com>
2025-04-01 13:21:54 -0400

    add color input widget

--- frontend/src/app/theme.component.ts
+++ frontend/src/app/theme.component.ts
@@ -8,8 +8,8 @@ import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
     <form class="container mt-4" (ngSubmit)="save()">
       @for (prop of settings; track prop.var) {
         <div class="mb-2">
+          <input [id]="prop.var" type="color" class="me-2" [formControl]="prop.control" placeholder="Hex code" />
           <label [for]="prop.var">{{prop.label}}</label>
-          <input [id]="prop.var" type="text" class="form-control" [formControl]="prop.control" placeholder="Hex code" />
         </div>
       }
 
@@ -27,37 +27,37 @@ export class ThemeComponent {
     {
       label: 'Navbar Color',
       var: '--navbar-color',
-      control: this.fb.control(''),
+      control: this.fb.control('#023047'),
     },
     {
       label: 'Header Color',
       var: '--heading-color',
-      control: this.fb.control(''),
+      control: this.fb.control('#023047'),
     },
     {
       label: 'Donate Button Background Color',
       var: '--donate-btn-background-color',
-      control: this.fb.control(''),
+      control: this.fb.control('#de5499'),
     },
     {
       label: 'Donate Button Border Color',
       var: '--donate-btn-border-color',
-      control: this.fb.control(''),
+      control: this.fb.control('#0d6efd'),
     },
     {
       label: 'Background color',
       var: '--background-color',
-      control: this.fb.control(''),
+      control: this.fb.control('#eddcd9'),
     },
     {
       label: 'Text background color',
       var: '--text-background-color',
-      control: this.fb.control(''),
+      control: this.fb.control('#f2ebe9'),
     },
     {
       label: 'Candidate names background color',
       var: '--names-background-color',
-      control: this.fb.control(''),
+      control: this.fb.control('#303179'),
     }
   ];
 
